f90f3c77e79ed8c7dbc1637ab68b1576fc5139c1
María José <marjose908@gmail.com>
2025-03-11 00:15:47 -0600

    Estilos y JavaScript

---
+++ src/main/resources/static/js/rutinas.js
@@ -0,0 +1,11 @@
+//La función carga localmente una imagen
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var lector = new FileReader();
+        lector.onload = function (e) {
+            $('#blah').attr('src',e.target.result)
+                    .height(200);
+        };
+        lector.readAsDataURL(input.files[0]);        
+    }
+}
