5c046efa6c473fb0b0bc59ad0ec8b80d41710614
María José <marjose908@gmail.com>
2025-04-21 11:52:05 -0600

    Creación de RolRepository y modificaciones en UsuarioService y UsuarioRepository

--- src/main/java/com/proyecto/repository/UsuarioRepository.java
+++ src/main/java/com/proyecto/repository/UsuarioRepository.java
@@ -3,9 +3,15 @@ package com.proyecto.repository;
 import com.proyecto.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository 
+public interface UsuarioRepository
         extends JpaRepository<Usuario, Long> {
+
     Usuario findByUsername(String username);
-    
+
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
 
 }
---
+++ src/main/java/com/proyecto/service/UsuarioService.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyecto.service;
+
+import com.proyecto.domain.Rol;
+import com.proyecto.domain.Usuario;
+import com.proyecto.repository.RolRepository;
+import com.proyecto.repository.UsuarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service 
+public class UsuarioService {
+    
+   @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private RolRepository rolRepository;
+
+    @Transactional(readOnly = true)
+    public List<Usuario> getUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario getUsuario(Usuario usuario) {
+        return usuarioRepository.findById(usuario.getIdUsuario()).orElse(null);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario getUsuarioPorUsername(String username) {
+        return usuarioRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario getUsuarioPorUsernameYPassword(String username, String password) {
+        return usuarioRepository.findByUsernameAndPassword(username, password);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario getUsuarioPorUsernameOCorreo(String username, String correo) {
+        return usuarioRepository.findByUsernameOrCorreo(username, correo);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean existeUsuarioPorUsernameOCorreo(String username, String correo) {
+        return usuarioRepository.existsByUsernameOrCorreo(username, correo);
+    }
+
+    @Transactional
+    public void save(Usuario usuario, boolean crearRolUser) {
+        usuario=usuarioRepository.save(usuario);
+        if (crearRolUser) {  //Si se está creando el usuario, se crea el rol por defecto "USER"
+            Rol rol = new Rol();
+            rol.setNombre("USER");
+            rol.setIdUsuario(usuario.getIdUsuario());
+            rolRepository.save(rol);
+        }
+    }
+
+    @Transactional
+    public void delete(Usuario usuario) {
+        usuarioRepository.delete(usuario);
+    }
+}
