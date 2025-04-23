fd910235cbb0199a95f33ae284848347af8aecd5
DanielaDiazA <danidiar.14@gmail.com>
2025-04-22 18:34:42 -0600

    carrito 2

--- src/main/java/com/proyecto/controller/CarritoController.java
+++ src/main/java/com/proyecto/controller/CarritoController.java
@@ -65,6 +65,7 @@ public class CarritoController {
 
     }
 
+    
     @GetMapping("/eliminar/{idProducto}")
     public String eliminar(Model model, Item item) {
         itemService.delete(item);
